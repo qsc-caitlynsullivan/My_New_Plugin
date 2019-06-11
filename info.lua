aa928dc93730d6eae5cd09d29c1b1f26206d1705
Caitlyn Sullivan <caitlynsullivan@qsc.com>
2019-06-11 16:56:04 -0400

    updated PluginInfo header

--- info.lua
+++ info.lua
@@ -2,6 +2,6 @@ PluginInfo = {
     Name = "My New Plugin",
     Version = "0.0.0.0",
     Id = "<guid>",
-    Description = "A New Plugin",
+    Description = "A New thing, maybe it'll work?",
     ShowDebug = true
 }
